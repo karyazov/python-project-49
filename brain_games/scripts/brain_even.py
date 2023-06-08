cdad2b93789e805a7c0dd86fdfeec34281df00ca
karv <karyazov@yandex.ru>
2023-06-08 13:08:15 +0300

    ошибка вывод имени пользователя

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,18 +2,18 @@
 import prompt
 import random
 
-user_name = ''
-
 
 def welcome_user():
     user_name = prompt.string('May I have your name? ')
     print('Hello, ' + user_name)
 
+    return user_name
+
 
 def main():
     print('Welcome to the Brain Games!')
 
-    welcome_user()
+    user_name = welcome_user()
 
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
